e06ae424f8aef3164f34785aa3d81d82fbccf28c
Justin Blake <justin@blaix.com>
2023-07-22 22:05:18 -0400

    elm language server and Browser.element

--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
 { pkgs ? import <nixpkgs> { } }:
 let
   mynode = pkgs.nodejs-18_x;
+  elmls = pkgs.elmPackages.elm-language-server;
 in
 pkgs.stdenv.mkDerivation {
   name = "my-shell";
   packages = [  ];
   shellHook = "";
-  buildInputs = [ mynode ];
+  buildInputs = [ mynode elmls ];
 }
--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,38 @@
 module Main exposing (main)
 
+import Browser
 import Html as H exposing (Html)
 
 
-main : Html msg
+main : Program () Model msg
 main =
-    H.text "Howdy"
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    Int
+
+
+init : () -> ( Model, Cmd msg )
+init _ =
+    ( 1, Cmd.none )
+
+
+update : msg -> Model -> ( Model, Cmd msg )
+update _ model =
+    ( model, Cmd.none )
+
+
+view : Model -> Html msg
+view _ =
+    H.text "You are cool."
+
+
+subscriptions : Model -> Sub msg
+subscriptions _ =
+    Sub.none
